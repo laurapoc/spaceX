c99772d31b187f76e94c7cbeabeda1345b589da2
Laura Pociuniene <laurapociuniene@gmail.com>
2021-11-09 12:46:30 +0200

    add loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Loader from "react-loader-spinner";
 
 import SearchRocket from "./components/searchRocket/SearchRocket";
 import { RocketDto } from "./types/rocketDto";
@@ -13,19 +14,19 @@ function App() {
   const classes = styles();
 
   const getRocketData = () => {
-      axios
-        .get<RocketDto[]>("https://api.spacexdata.com/v3/rockets")
-        .then((response) => {
-          const data = response.data;
-          setRockets(data);
-          setloading(false);
-        })
-        .catch((err) => {
-          if (axios.isCancel(err)) return;
-          setloading(false);
-          setError(true);
-          return err;
-        });
+    axios
+      .get<RocketDto[]>("https://api.spacexdata.com/v3/rockets")
+      .then((response) => {
+        const data = response.data;
+        setRockets(data);
+        setloading(false);
+      })
+      .catch((err) => {
+        if (axios.isCancel(err)) return;
+        setloading(false);
+        setError(true);
+        return err;
+      });
   };
 
   useEffect(() => {
@@ -38,8 +39,20 @@ function App() {
     <div className={classes.app}>
       <div className={classes.appContainer}>
         <SearchRocket {...{ rockets }} />
-        <div className={classes.notification}>{loading && "Loading..."}</div>
-        <div className={classes.notification}>{error && "Something went wrong..."}</div>
+        <div className={classes.notification}>
+          {loading && (
+            <Loader
+              type="ThreeDots"
+              color="#FFFFFF"
+              height={80}
+              width={80}
+              timeout={3000}
+            />
+          )}
+        </div>
+        <div className={classes.notification}>
+          {error && "Something went wrong..."}
+        </div>
       </div>
     </div>
   );
